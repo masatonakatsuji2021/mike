00a4d5a6e778036c4a3487943de4dd44313c9bb8
nakajima-satoru <nakajima@email.com>
2022-01-22 00:38:44 +0900

    1/21

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
  * License : MIT License. 
  * Since   : 2022.01.15
  * Author  : Nakatsuji Masato 
+ * Email   : nakatsuji@teastalk.jp
+ * HP URL  : https://hachiware-js.com/
  * GitHub  : https://github.com/masatonakatsuji2021/hachiware_server_module_framework
  * npm     : https://www.npmjs.com/package/hachiware_server_module_framework
  * =========================================================================================
